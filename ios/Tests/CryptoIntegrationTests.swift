3477f374f51843068cba327504a3f9163b9e2ea2
semihtakilan <semihtakilan16@gmail.com>
2026-09-01 18:15:02 +0300

    test: fix iOS integration test target

--- ios/Tests/CryptoIntegrationTests.swift
+++ ios/Tests/CryptoIntegrationTests.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import XCTest
+@testable import AES128CryptoEngine
 
 final class CryptoIntegrationTests: XCTestCase {
     func testEncryptThenDecryptReturnsPlaintext() throws {
